47f3ce40be711352f3e90c160d614147ba816aa9
Carlos-Caravaca <carlosc938@gmail.com>
2023-08-30 14:54:37 -0600

    Creando primeros archivos

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # IE0624_B61512
 Este repositorio será utilizado para el curso IE-0624 Laboratorio de Microcontroladores
+
+**Informacion**
+- Nombre: Carlos A. Caravaca Mora
+- Carne: B61512
---
+++ lab_01.c
@@ -0,0 +1,16 @@
+/*
+Autor: Carlos A. Caravaca
+Fecha: 2 septiembre
+Título: Laboratorio 01
+----------------------------------------------------------------
+Descripción:
+El proyecto propone un simulador de dado simple 
+que utiliza LEDs para mostrar resultados. Emplea 
+un microcontrolador PIC12F683 y un botón como entrada. 
+Al presionar el botón, el microcontrolador genera un número 
+aleatorio que representa el resultado del lanzamiento de un 
+dado. Los LEDs se activan en secuencia para mostrar este número 
+de manera visual, imitando las caras de un dado convencional.
+*/
+
+#include <pic14/pic12f683.h>

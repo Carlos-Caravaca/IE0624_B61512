044b8b44a6b36670c74cd0c40997d0d66534394c
sanchoerick <84211587+sanchoerick@users.noreply.github.com>
2023-09-17 00:18:40 -0600

    Estado WAIT 1.5
    Signed-off-by: sanchoerick <84211587+sanchoerick@users.noreply.github.com>

--- lab_02/src/lab_02.c
+++ lab_02/src/lab_02.c
@@ -66,7 +66,8 @@ int main(void) {
         PORTD &= ~(1 << LED_PD5);   //rojo vehiculo apagado
         PORTB &= ~(1 << LED_PB2);   //verde peaton apagado
         PORTB &= ~(1 << LED_PB0);   //amarillo apagado
-        
+        Button_IsPressed();
+
         if (WAIT)
         {
             timer01_ms(10000);

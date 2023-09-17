af73d37b7ea613dcdc7c43274c75585ecb5ff8cb
Carlos_Caravaca <carlos.caravaca@ucr.ac.cr>
2023-09-16 23:03:10 -0600

    codigo y simulacion casi completos

--- lab_02/src/lab_02.c
+++ lab_02/src/lab_02.c
@@ -1,63 +1,116 @@
-
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
-#define LED_PIN PB0
-#define BUTTON_PIN PB6
+#define LED_PB0 PB0     //rojo vehiculo
+#define LED_PB1 PB1     //verde vehiculo
+#define LED_PB2 PB2     //verde peaton
+#define LED_PB3 PB3     //rojo peaton
+#define LED_PD5 PD5     //amarillo
+#define BUTTON_PIN PB6  //boton
 
 volatile uint32_t mili_seg = 0;
-volatile uint8_t boton_presionado = 0;
 
-// Función de inicialización del Timer1
+// función de inicialización del Timer1
 void Timer1_Init() {
-    TCCR1A = 0;             // Modo normal de operación
-    TCCR1B = (1 << WGM12);  // Configura el temporizador en modo CTC
-    OCR1A = 249;            // Valor para comparación (249 ciclos, asumiendo una frecuencia de reloj de 16 MHz y preescalador de 64)
-    TIMSK |= (1 << OCIE1A); // Habilita la interrupción por comparación con OCR1A
+    TCCR1A = 0;             // modo normal de operación
+    TCCR1B = (1 << WGM12);  // configura el temporizador en modo CTC
+    OCR1A = 249;            // valor para comparación (249 ciclos, asumiendo una frecuencia de reloj de 16 MHz y preescalador de 64)
+    TIMSK |= (1 << OCIE1A); // habilita la interrupción por comparación con OCR1A
     TCCR1B |= (1 << CS11) | (1 << CS10); // Configura el preescalador a 64
 }
 
-// Rutina de interrupción del Timer1 (OCIE1A)
+// rutina de interrupción del Timer1 (OCIE1A)
 ISR(TIMER1_COMPA_vect) {
-    mili_seg++; // Incrementa el contador de milisegundos en cada interrupción
+    mili_seg++; // incrementa el contador de milisegundos en cada interrupción
 }
 
-// Función para esperar un número de milisegundos usando Timer1
-void time1_delay(uint32_t ms) {
+// función para esperar un número de milisegundos usando Timer1
+void timer01_ms(uint32_t ms) {
     uint32_t start_time = mili_seg;
     while (mili_seg - start_time < ms) {
-        // Espera hasta que se alcance el tiempo deseado
+        // espera hasta que se alcance el tiempo deseado
     }
 }
 
-// Función para inicializar el botón como entrada con resistencia pull-up
+// función para inicializar el botón como entrada con resistencia pull-up
 void Button_Init() {
-    DDRB &= ~(1 << BUTTON_PIN); // Configura el pin del botón como entrada
-    PORTB |= (1 << BUTTON_PIN); // Habilita la resistencia pull-up para el botón
+    DDRB &= ~(1 << BUTTON_PIN); // configura el pin del botón como entrada
+    PORTB |= (1 << BUTTON_PIN); // habilita la resistencia pull-up para el botón
 }
 
-// Función para verificar si el botón está presionado
+// función para verificar si el botón está presionado
 uint8_t Button_IsPressed() {
-    return !(PINB & (1 << BUTTON_PIN)); // Retorna 1 si el botón está presionado
+    // retorna 1 si el botón está presionado
+    return !(PINB & (1 << BUTTON_PIN)); 
 }
 
 int main(void) {
-    DDRB |= (1 << LED_PIN); // Configura el pin del LED como salida
-    Timer1_Init(); // Inicializa Timer1
-    Button_Init(); // Inicializa el botón
-    sei(); // Habilita las interrupciones globales
-    PORTB |= (1 << LED_PIN);
+    // se coonfiguran los pines de los LEDs como salidas
+    DDRB |= (1 << LED_PB0); 
+    DDRB |= (1 << LED_PB1);
+    DDRB |= (1 << LED_PB2);
+    DDRB |= (1 << LED_PB3);
+    DDRD |= (1 << LED_PD5);
+
+    Timer1_Init();      // inicializa Timer1
+    Button_Init();      // inicializa el botón
+    sei();              // Habilita las interrupciones globales
 
     while (1) {
-        if (Button_IsPressed()) {
-            boton_presionado = 1; // Indica que el botón ha sido presionado
-        }
+        // Se establece el estado inicial en verde para vehiculos
+        // y rojo para peatones
+        PORTB |= (1 << LED_PB1);    //verde vehiculo endendido
+        PORTB |= (1 << LED_PB3);    //rojo peaton encendido
+        PORTD &= ~(1 << LED_PD5);   //rojo vehiculo apagado
+        PORTB &= ~(1 << LED_PB2);   //verde peaton apagado
+        PORTB &= ~(1 << LED_PB0);   //amarillo apagado
+        timer01_ms(10000);
 
-        if (boton_presionado) {
-            PORTB ^= (1 << LED_PIN); // Invierte el estado del LED
-            time1_delay(1000); // Espera 1000 ms (1 segundo)
+        // se debe mantener pulsado el boton hasta que inicie 
+        // la secuencia de luces, en ese momento ya se puede soltar
+        if (Button_IsPressed()) {
+            // LDPV parpedeando durante 3 segundos
+            // junto conluz amarilla
+            PORTB &= ~(1 << LED_PB3);
+            PORTB &= ~(1 << LED_PB1);
+            PORTB &= ~(1 << LED_PB0); 
+            timer01_ms(500);
+            PORTB |= (1 << LED_PB1);
+            PORTB |= (1 << LED_PB0);
+            timer01_ms(500);
+            PORTB &= ~(1 << LED_PB1);
+            PORTB &= ~(1 << LED_PB0); 
+            timer01_ms(500);
+            PORTB |= (1 << LED_PB1);
+            PORTB |= (1 << LED_PB0);
+            timer01_ms(500);
+            PORTB &= ~(1 << LED_PB1);
+            PORTB &= ~(1 << LED_PB0); 
+            timer01_ms(500);
+            PORTB |= (1 << LED_PB1);
+            PORTB |= (1 << LED_PB0);
+            timer01_ms(500);
+            PORTB &= ~(1 << LED_PB1);
+            PORTB &= ~(1 << LED_PB0); 
+            // se enciende verde peaton
+            // se enciende rojo vehiculo
+            PORTB |= (1 << LED_PB2);
+            PORTD |= (1 << LED_PD5);
+            timer01_ms(15000);
+
+            // regresa a su estado inicial
+            PORTB |= (1 << LED_PB1);    //verde vehiculo endendido
+            PORTB |= (1 << LED_PB3);    //rojo peaton encendido
+            PORTD &= ~(1 << LED_PD5);   //rojo vehiculo apagado
+            PORTB &= ~(1 << LED_PB2);   //verde peaton apagado
+            PORTB &= ~(1 << LED_PB0);   //amarillo apagado
+
+            // Espera hasta que se libere el botón para evitar múltiples pulsaciones
+            while (Button_IsPressed()) {}
         }
     }
 
     return 0;
+
+    
 }

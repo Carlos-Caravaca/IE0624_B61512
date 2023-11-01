f7e2b69b968364b5948cd3741d4d0958390ab5ec
sanchoerick <84211587+sanchoerick@users.noreply.github.com>
2023-10-31 21:21:02 -0600

    Actualizacion

--- lab_04/src/dashboard.py
+++ lab_04/src/dashboard.py
@@ -1,39 +1,36 @@
 import serial
 import requests
-import json
-import ssl, socket, time
-import paho.mqtt.client as mqtt
+import time
 
-# Configura el puerto serial/USB
-ser = serial.Serial('/dev/ttyUSB0', 9600)  # Reemplaza con el puerto correcto
+# Configuración del puerto serial/USB
+# ser = serial.Serial('/dev/ttyUSB0', 9600) 
 
 # Configura la URL y el token de acceso de ThingsBoard
 access_token = 'mtzd36o3ynydaptt4ywh'
-url = f'https://iot.eie.ucr.ac.cr/api/v1/{access_token}/telemetry'
 
-# Configuración de la comunicación serial (ajusta el puerto según tu sistema)
-ser = serial.Serial('/dev/ttyUSB0', 115200, timeout=1)
-
-# URL del servidor ThingsBoard (ajusta la URL y el token de acceso)
+# URL del servidor ThingsBoard 
 thingsboard_url = "https://iot.eie.ucr.ac.cr"
+
 
 def send_data_to_thingsboard(data):
     headers = {'Content-Type': 'application/json'}
     payload = {"ts": int(time.time() * 1000), "values": data}
     url = f"{thingsboard_url}/api/v1/{access_token}/telemetry"
     response = requests.post(url, json=payload, headers=headers)
-    
+
     if response.status_code == 200:
         print("Datos enviados exitosamente a ThingsBoard")
     else:
         print("Error al enviar datos a ThingsBoard")
 
+
 try:
+    ser = serial.Serial('COM2', 9600)  # 'COM3' puerto serie 
     while True:
-        # Leer datos del puerto serial (ajusta según tu protocolo de comunicación)
+        # Leer datos del puerto serial
         serial_data = ser.readline().decode().strip()
-        
-        # Procesar los datos del giroscopio y nivel de batería (ajusta según tus datos)
+
+        # Procesar los datos del giroscopio y nivel de batería
         gyro_data = {"gyro_x": 123, "gyro_y": 456, "gyro_z": 789}
         battery_data = {"battery_voltage": 5.46}
 
